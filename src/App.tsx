8b41aa99e8d1d599cf455b1a261ca628f37e5a9a
Jinhee <ooaaxx3@gmail.com>
2023-07-19 16:01:53 +0900

    3-5까지

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hellow", value);
+  };
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="ggg" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          type="text"
+          placeholder="username"
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
